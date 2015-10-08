aa3882f552c2e04563cbf65b2ea48fc48677f57c
Honza <schaumann.jan@gmail.com>
2015-10-08 20:20:49 +0200

    testik

---
+++ src/OverKill.java
@@ -0,0 +1,14 @@
+import java.util.logging.Logger;
+
+/**
+ * Created by schaumanni on 10/8/15.
+ */
+public class OverKill {
+
+    private static final Logger log = Logger.getLogger(OverKill.class.getName());
+
+    public static void main(String[] args) {
+        log.info("Hello fun will Begin");
+    }
+
+}
